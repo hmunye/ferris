cbfecf814e85dd1c7ca66e28c73eaac8d9f48029
Huseen <136820327+hmunye@users.noreply.github.com>
2025-11-10 17:59:39 -0500

    casual attention with dropout

--- src/attention.py
+++ src/attention.py
@@ -1,21 +1,37 @@
 import torch
 
 class SelfAttention(torch.nn.Module):
-    def __init__(self, d_in, d_out, qkv_bias=False):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
+        self.d_out = d_out
+
         self.w_q = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
         self.w_k = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
         self.w_v = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = torch.nn.Dropout(dropout)
+
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
 
     def forward(self, input):
+        b, num_tokens, d_in = input.shape
+
         k = self.w_k(input)
         q = self.w_q(input)
         v = self.w_v(input)
 
-        attn_scores = q @ k.T
-        attn_weights = torch.nn.functional.softmax(
-            attn_scores / k.shape[-1]**0.5, dim=-1
+        attn_scores = q @ k.transpose(1, 2)
+        # Performed in-place.
+        attn_scores.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], 
+            -torch.inf
         )
+
+        attn_weights = self.dropout(torch.nn.functional.softmax(
+            attn_scores / k.shape[-1]**0.5, dim=-1
+        ))
 
         context = attn_weights @ v
         return context
--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ def main():
     import torch
     from attention import SelfAttention 
     
+    torch.manual_seed(123)
+
     inputs = torch.tensor(
         [[0.43, 0.15, 0.89],
          [0.55, 0.87, 0.66],
@@ -13,11 +15,13 @@ def main():
 
     d_in = inputs.shape[1]
     d_out = 2
+    batch = torch.stack((inputs, inputs), dim=0)
+    context_length = batch.shape[1]
 
-    torch.manual_seed(789)
-    attn = SelfAttention(d_in, d_out)
+    attn = SelfAttention(d_in, d_out, context_length, 0.0)
+    context_vecs = attn(batch)
 
-    print(attn(inputs))
+    print("context shape:", context_vecs.shape)
 
 
 if __name__ == "__main__":
