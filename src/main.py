a736f5889d4a0ddcf7099a2c4509495bd5a4d6e9
Huseen <136820327+hmunye@users.noreply.github.com>
2025-11-10 16:15:46 -0500

    simple self-attention mechanism

--- src/main.py
+++ src/main.py
@@ -1,18 +1,27 @@
 def main():
     import torch
-    import torch.nn.functional as F
+    
+    inputs = torch.tensor(
+        [[0.43, 0.15, 0.89],
+         [0.55, 0.87, 0.66],
+         [0.57, 0.85, 0.64],
+         [0.22, 0.58, 0.33],
+         [0.77, 0.25, 0.10],
+         [0.05, 0.80, 0.55]]
+    )
 
-    y = torch.tensor([1.0])
+    attn_scores = inputs @ inputs.T
 
-    x1 = torch.tensor([1.1])
+    print(f"scores: {attn_scores}\n")
+    
+    attn_weights = torch.nn.functional.softmax(attn_scores, dim=-1)
 
-    w1 = torch.tensor([2.2])
-    b = torch.tensor([0.0])
+    print(f"weigths: {attn_weights}\n")
 
-    z = x1 * w1 + b
-    a = torch.sigmoid(z)
+    context_vecs = attn_weights @ inputs
 
-    loss = F.binary_cross_entropy(a, y)
+    print("contexts:", context_vecs)
+            
 
 if __name__ == "__main__":
    main()
